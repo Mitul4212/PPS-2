6e623175d57fc024ff76e638a80f314896a959c6
Mitul4212 <100231240+Mitul4212@users.noreply.github.com>
2022-05-26 11:42:01 +0530

    Add files via upload

---
+++ s3/c7.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int convert(int a){
+	return (int)a;
+}
+
+float convert(char a){
+	return (float)a;
+}
+
+int main(){
+	char a;
+	cout<<convert('a')<<endl;;
+	cout<<convert('a');	
+}
