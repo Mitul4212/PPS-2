627bf37c40c3df60847022b7a572c5292cacc621
Mitul4212 <100231240+Mitul4212@users.noreply.github.com>
2022-06-09 17:25:11 +0530

    Update c2.cpp

--- s4/c2.cpp
+++ s4/c2.cpp
@@ -6,7 +6,11 @@ class item{
 	float cost;
 	int qty;
 	float total_price;
+    static float sum;
 public:
+    item(){
+        sum=0;
+    }
 	void getdata(){
 		cout<<"Enter product code : ";
 		cin>>code;
@@ -17,18 +21,31 @@ public:
 	}
 	void calculate(){
 		total_price=qty*cost;
+        sum+=total_price;
 	}
 	void dispdata(){
 		cout<<"product code 	: "<<code<<endl;
 		cout<<"product qty	: "<<qty<<endl;
 		cout<<"product price 	: "<<cost<<endl;
 		cout<<"Total price 	: "<<total_price<<endl;
-	}
+        
+    }
+    void disptotal(){
+        cout<<"All over Total Amount : "<<sum;
+    }
 };
 
+float item::sum=0;
+
 int main(){
-	item p1;
-	p1.getdata();
-	p1.calculate();
-	p1.dispdata();
+    int n;
+    cout<<"How many Books diteils : ";
+    cin>>n;
+	item p[n];
+	for(int i=0;i<n;i++){
+        p[i].getdata();
+	    p[i].calculate();
+	    p[i].dispdata();
+    }
+    p[0].disptotal();
 }
